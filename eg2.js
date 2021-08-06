8e99dccf6c5f1cbb1f41846e931f0b1e16d5f74d
Aditya <adityalalwani05@gmail.com>
2021-08-06 12:50:53 +0530

    Initial Commit

---
+++ eg2.js
@@ -0,0 +1,12 @@
+function f1() {
+	console.log("Hi by f1!");
+}
+
+function f2() {
+	f1();
+	console.log("Hi by f2!");
+}
+
+f2();
+
+//Src: https://www.geeksforgeeks.org/what-is-the-call-stack-in-javascript/
---
+++ eg3.js
@@ -0,0 +1,8 @@
+var x = 7;
+
+function getName() {
+	console.log("hello");
+}
+
+getName();
+console.log(x);
